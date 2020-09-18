6daa064693927a806566d70c4568770b784cdcf1
= <goyal.kumarabhishek@gmail.com>
2020-09-18 13:08:32 +0530

    playlist size

--- Youtube_Channel_Details.py
+++ Youtube_Channel_Details.py
@@ -3,7 +3,8 @@ import re
 import requests
 from pprint import pprint
 from datetime import timedelta
-from Video_Crawler import youtube_video_download
+from Video_Crawler import youtube_video_download,yotube_video_size
+
 
 api_key = 'AIzaSyCFs-0PRFSEiRqPqXoorAXoxR8p7e03jvM'
 
@@ -11,7 +12,6 @@ youtube = build('youtube','v3',developerKey=api_key)
 
 # Youtube channel detail
 class YoutubeChannelDetails:
-
 	def __init__(self,channel_url):
 		self.channel_url=channel_url
 		self.status = requests.get(channel_url).status_code
@@ -95,7 +95,7 @@ class PlayListDetails:
 					playlist_url=playlist_url[len(url_pattern[1]):]
 					self.playlist_id=playlist_url[(playlist_url.index("=")+1):]
 				else:
-					print("No URL Found!")
+					print("No Playlist Found!")
 				while True:
 					pl_request = youtube.playlistItems().list(
 						part='contentDetails',
@@ -114,7 +114,7 @@ class PlayListDetails:
 		except:
 			return 
 
-	# number of videos in playlist
+	# Tells total number of videos in playlist
 	def NumberOfVideos(self):
 		if self.status==200:
 			try:
@@ -161,25 +161,107 @@ class PlayListDetails:
 			return "Invalid URL!"
 
 	def PlaylistAllVideos(self):
-		for video in self.video_ids:
-			self.video_links.append(f"https://www.youtube.com/watch?v={video}")
-		return self.video_links
-
-	def DownloadPlaylistVideos(self,number_of_videos=None):
-		self.PlaylistAllVideos()
-		if number_of_videos==None:
-			print(self.video_links)
+		if self.video_ids!=[]:
+			for video in self.video_ids:
+				self.video_links.append(f"https://www.youtube.com/watch?v={video}")
+			return self.video_links
 		else:
-			if isinstance(number_of_videos,int):
-				if number_of_videos == 0:
-					print("Invalid Number!")
-				elif number_of_videos>len(self.video_links):
-					print(f"In this playlist only {len(self.video_links)} videos are there!") 
+			return "No Videos Found!"
+
+	# Atrributes:-
+		# 1.no_of_videos
+		# 2.froms
+		# 3.tos
+		# 4.list_of_video
+	# 1. no_of_videos:-how number of videos you want to downlaod
+	# 2. froms:-from which video you want to downlaod
+	# 3. tos:-till which video you want to downlaod
+	# 4. if you want to downlaod only one specific video from playlist ,pass same value in froms and tos
+	# 5. if you want to downlaod only specific number of video ,pass list of number in list_of_video
+
+	# Example
+	# In Playlist 4 videos are there(v1,v2,v3,v4)
+	# 1. For download all videos ,don't pass any attribute
+	# 2. for download 2 videos ,pass 2 in no_of_videos (no_of_videos=2) It will downlaod v1 and v2
+	# 3. for downlaod 2 and 3rd video, pass 2 in froms and 3 in tos (froms=2,tos=3), it will downlaod v2 and v3
+	# 4. for download all videos from 2nd video, pass 2 in froms (froms=2), it will download v2,v3 and v4.
+	# 5. for download v1 and v4 video, pass [1,4] in list_of_video (list_of_video=[1,4]), it will download only v1 and v4
+	def DownloadPlaylistVideos(self,**kwargs):
+		no_of_videos,froms,tos=None,None,None
+		if 'no_of_videos' in kwargs.keys():
+			no_of_videos = kwargs['no_of_videos']
+		if 'froms' in kwargs.keys():
+			froms = kwargs['froms']
+		if 'tos' in kwargs.keys():
+			tos = kwargs['tos']
+		if 'list_of_video' in kwargs.keys():
+			list_of_video = kwargs['list_of_video']
+		self.PlaylistAllVideos()
+		if no_of_videos==None and froms==None and tos==None and list_of_video==None:
+			youtube_video_download(self.video_links)
+		else:
+			if no_of_videos!=None and froms==None and tos==None and list_of_video==None:
+				if isinstance(no_of_videos,int):
+					if no_of_videos == 0:
+						print("Invalid Number!")
+					elif no_of_videos>len(self.video_links):
+						print(f"In this playlist only {len(self.video_links)} videos are there!") 
+					else:
+						youtube_video_download(self.video_links[0:no_of_videos])
 				else:
-					print(self.video_links[0:number_of_videos])
+					print("Enter Number Only!")
+			elif froms!=None and no_of_videos==None and tos==None and list_of_video==None:
+				if isinstance(froms,int):
+					if froms == 0:
+						print("Invalid Number!")
+					elif froms>len(self.video_links):
+						print(f"In this playlist only {len(self.video_links)} videos are there!") 
+					else:
+						youtube_video_download(self.video_links[froms-1:])
+				else:
+					print("Enter Number Only!")
+			elif froms!=None and no_of_videos==None and tos!=None and list_of_video==None:
+				if isinstance(froms,int) and isinstance(tos,int) :
+					if froms == 0 or tos==0:
+						print("Invalid Number!")
+					elif froms>len(self.video_links) or tos>len(self.video_links):
+						print(f"In this playlist only {len(self.video_links)} videos are there!") 
+					elif froms>tos:
+						print("froms can not be greater!")
+					else:
+						youtube_video_download(self.video_links[froms-1:tos])
+				else:
+					print("Enter Number Only!")
+			elif froms==None and no_of_videos==None and tos==None and list_of_video!=None:
+				if isinstance(list_of_video,list):
+					if list_of_video!=[]:
+						for video in list_of_video:
+							if not isinstance(video,int):			
+								print("Enter Number Only!")
+							elif video==0:
+								print("Invalid Number!")
+							elif video>len(self.video_links):
+								print(f"In this playlist only {len(self.video_links)} videos are there!") 
+							else:
+								youtube_video_download(self.video_links[video-1])
+					else:
+						print("list is empty!")
+				else:
+					print("Pass Number in list")
 			else:
-				print("Enter Number Only!")
+				print("No attribute found!")
+
+	# Size of playlist in Bytes
+	def PlaylistSize(self):
+		video_size=0
+		self.PlaylistAllVideos()
+		for video_link in self.video_links:
+			video_size+=yotube_video_size(video_link)
+		return video_size
 
 
-y = PlayListDetails('https://www.youtube.com/playlist?list=PLsyeobzWxl7r2ukVgTqIQcl-1T0C2mzau')
-y=y.DownloadPlaylistVideos()
+y = PlayListDetails('https://www.youtube.com/playlist?list=')
+print(y.PlaylistSize())
+
+	
+# y.DownloadPlaylistVideos(list_of_video=[10])
